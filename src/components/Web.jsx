b6e3513b2d2e4acc885afe5bb50f7917754c17db
Dmitrii Ermolaev <bugiman532@gmail.com>
2023-05-23 15:42:06 +0300

    initial commit

---
+++ src/components/Web.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../assets/web.scss';
+
+function Web() {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <Header />
+        <Aside />
+        <Main />
+      </div>
+    </div>
+  );
+}
+
+export default Web;
